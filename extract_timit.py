a6d0e0ac8e8027dca5ec0c295e6902990b53306f
Peter Smit <peter.smit@aalto.fi>
2015-08-17 15:57:32 +0300

    Change the normalization to zero-mean unit-variance

--- extract_timit.py
+++ extract_timit.py
@@ -25,8 +25,8 @@ def main(dir):
     train_labels = []
     for wav_file, phn_file in zip(sorted(wav_files),
                                   sorted(phn_files)):
-        print(wav_file)
-        print(phn_file)
+        #print(wav_file)
+        #print(phn_file)
         with tempfile.TemporaryFile() as fp:
             subprocess.check_call(['sox', wav_file, '-t', 'wav', '-'], stdout=fp)
             fp.seek(0)
@@ -36,7 +36,9 @@ def main(dir):
 
             # what kind of window width and overlap should we use?
             spec, freqs, t = matplotlib.mlab.specgram(data_f, 400, Fs=1, window=np.hamming(400), noverlap=200)
-            train_samples = np.append(train_samples, normalize(spec.T,axis=1,norm='l1'), axis=0)
+            train_samples = np.append(train_samples, spec.T, axis=0)
+
+
 
             t = list(t)
             for line in open(phn_file):
@@ -46,7 +48,13 @@ def main(dir):
                     t = t[1:]
 
         while train_samples.shape[0] > len(train_labels):
-            train_labels.append("UNKNOWN")
+            print(phn_file)
+            train_labels.append("h#")
+
+
+    train_samples = (train_samples - train_samples.mean(axis=0)) / train_samples.std(axis=0)
+
+    print(train_samples.std(axis=0))
 
     print(train_samples.shape)
     print(len(train_labels))
